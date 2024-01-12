81b595ddc53b8d7a8b4f281b88d078849bc5b5a9
seema-pr <seema.priyadarshinee@gmail.com>
2024-01-12 13:42:47 +0530

    validation added in util

--- Project1_CRUD/src/controllers/usersController.ts
+++ Project1_CRUD/src/controllers/usersController.ts
@@ -1,36 +1,8 @@
-// usersController.ts
+
 import { Request, Response } from 'express';
 import { getUsers, User } from '../models/users';
 import { v4 as uuidv4 } from 'uuid';
-
-// Import UUID validation library
-const validate = require('uuid-validate');
-
-// Function to validate a UUID
-const validateUUID = (userId: string): boolean => {
-    const isValidUUID = validate(userId);
-    return isValidUUID;
-};
-
-// Function to validate the required fields in the request body
-const validateRequestBody = (req: Request): string[] => {
-    const { username, age, hobbies } = req.body;
-    const missingFields = [];
-
-    if (!username) {
-        missingFields.push('username');
-    }
-
-    if (!age) {
-        missingFields.push('age');
-    }
-
-    if (!hobbies) {
-        missingFields.push('hobbies');
-    }
-
-    return missingFields;
-};
+import { validateUUID, validateRequestBody } from '../utils/validator';
 
 // Get the users data 
 const users = getUsers();
